5a765f99d3b1905a950a02f47dacdabc2c59b720
Anik <anik.khondokar09@gmail.com>
2026-06-01 18:44:01 +0600

    loops

--- 05_conditional_loop.py
+++ 05_conditional_loop.py
@@ -7,4 +7,40 @@ elif num>0:
 else:    
     print(f"Negative {num}")   
 
-#loop
+#for loop
+# for index_Value  in iterator:
+#     ->do something
+print("First way....")
+score=[23,45,34,56,787,77,345]
+for i in [1,2,3,4]:
+    print(f"Score is = {score[i]}")
+print("\n")
+
+
+#range() is a function
+#range(start,end,stepsize)
+#Another way:
+print("Second way..")
+for i in range(0,len(score), 2):
+    print(f"Values : {score[i]}")
+print('\n')
+
+#Another way:
+print("Third way..")
+for num in score:
+    print(num)
+
+## iterator and iterable
+print("\niterator and iterable..")
+numbers=[12,34,56,78,45]
+i=iter(numbers)
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+
+print("\n Enumerate in loops it needed in cross validation..")
+for index,value in enumerate(numbers):
+    print(index,value)
+
+
