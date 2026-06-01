878cc5db1878800186f788e190fe5ff0166837a1
Anik <anik.khondokar09@gmail.com>
2026-06-01 17:50:08 +0600

    conditional

---
+++ 05_conditional_loop.py
@@ -0,0 +1,10 @@
+#conditional statement
+num=int(input('Enter a number: '))
+if num==0:
+    print(f"Zero {num}")
+elif num>0:    
+    print(f"Positive {num}")
+else:    
+    print(f"Negative {num}")   
+
+#loop
